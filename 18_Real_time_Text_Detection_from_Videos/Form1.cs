311264b724a70be739a1d7c58b827fbbbdad0d69
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-03-19 10:17:58 +0100

    First commit

---
+++ 18_Real_time_Text_Detection_from_Videos/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18_Real_time_Text_Detection_from_Videos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
